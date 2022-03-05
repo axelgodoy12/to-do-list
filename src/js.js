71f26d93247bbf8aef4b57872ac8361527082f98
axelgodoy12 <84821493+axelgodoy12@users.noreply.github.com>
2022-03-05 20:23:01 -0300

    changes

---
+++ src/js.js
@@ -0,0 +1,11 @@
+// referencias de pantallas
+let pantallaBienvenida = document.getElementById("pantallaBienvenida");
+let pantallaFormulario = document.getElementById("pantallaFormulario");
+let pantallaListado = document.getElementById("pantallaListado");
+let pantallaDetalle = document.getElementById("pantallaDetalle");
+
+// button-accion agregarProducto
+let agregarProducto = document.getElementById("agregarProducto");
+agregarProducto.addEventListener('click', () => {
+
+})
