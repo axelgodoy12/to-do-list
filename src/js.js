dc2f0cde6bd8297b1e6a55465d5d98444083701b
axelgodoy12 <84821493+axelgodoy12@users.noreply.github.com>
2022-03-05 21:48:09 -0300

    update

--- src/js.js
+++ src/js.js
@@ -3,9 +3,28 @@ let pantallaBienvenida = document.getElementById("pantallaBienvenida");
 let pantallaFormulario = document.getElementById("pantallaFormulario");
 let pantallaListado = document.getElementById("pantallaListado");
 let pantallaDetalle = document.getElementById("pantallaDetalle");
+let formTitle = document.getElementById("formTitle");
+let formDescripcion = document.getElementById("formDescripcion");
+let addTask = document.getElementById("addTask");
+let listTable = document.getElementById("historyTable");
 
 // button-accion agregarProducto
 let agregarProducto = document.getElementById("agregarProducto");
 agregarProducto.addEventListener('click', () => {
+    pantallaBienvenida.style.display = "none";
+    pantallaFormulario.style.display = "block";
+})
 
+let mostrarListado = () => {
+    pantallaFormulario.style.display = "none";
+    pantallaListado.style.display = "block";
+}
+
+addTask.addEventListener("click", () => {
+    let title = formTitle.value;
+    let descripcion = formDescripcion.value;
+    let modelo = `<td>${title}</td>
+                    <td>${descripcion}</td>`;
+    historyTable.innerHTML += modelo;
+    mostrarListado();
 })
