1fc0db6248f890eb543085f55c0afb5f016e205b
axelgodoy12 <84821493+axelgodoy12@users.noreply.github.com>
2022-03-08 13:38:27 -0300

    update js

---
+++ src/app.js
@@ -0,0 +1,116 @@
+// referencias de pantallas
+let pantallaBienvenida = document.getElementById("pantallaBienvenida");
+let pantallaFormulario = document.getElementById("pantallaFormulario");
+let pantallaListado = document.getElementById("pantallaListado");
+let pantallaDetalle = document.getElementById("pantallaDetalle");
+let agregarProductoListado = document.getElementById("agregarProductoListado");
+let agregarProducto = document.getElementById("agregarProducto");
+let exitButton = document.getElementById("exitButton");
+const formTask = document.getElementById("formTask");
+const formTitle = document.getElementById("formTitle");
+const listTask = document.getElementById("listTask");
+const template = document.getElementById("template").content;
+const fragment = document.createDocumentFragment();
+let tasks = {};
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('tasks')) {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    addTask();
+})
+// console.log(Date.now())
+
+listTask.addEventListener('click', (e) => {
+    btnAdd(e);
+})
+
+let mostrarBienvenida = () => {
+    pantallaFormulario.style.display = "none";
+    pantallaBienvenida.style.display = "block";
+};
+let addTask2 = () => {
+    pantallaBienvenida.style.display = "none";
+    pantallaFormulario.style.display = "block";
+    pantallaListado.style.display = "none";
+}
+agregarProducto.addEventListener('click', addTask2);
+exitButton.addEventListener("click", mostrarBienvenida);
+
+formTask.addEventListener('submit', e => {
+    e.preventDefault();
+    // FORMAS DE SELECCIONAR LOS VALORES
+    // console.log(e.target[0].value);
+    // console.log(e.target.querySelector("input").value);
+    // console.log(input.value);
+
+    setTask(e)
+})
+
+const setTask = (e) => {
+    if (formTitle.value.trim() === "") {
+        alert("Completa los campos");
+        return
+    }
+    const task = {
+        id: Date.now(),
+        texto: formTitle.value,
+        estado: false
+    }
+    tasks[task.id] = task
+    formTask.reset();
+    formTitle.focus();
+
+    addTask();
+}
+
+const addTask = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    if (Object.values(tasks).length === 0) {
+        listTask.innerHTML = `
+            <div class="alert alert-dark text-center">
+                No hay tareas pendietes 👌👌
+            </div> `;
+        return
+    }
+    listTask.innerHTML = "";
+    Object.values(tasks).forEach(task => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('p').textContent = task.texto
+        if (task.estado) {
+            clone.querySelector('.taskItems').classList.add('taskItemsConteiner');
+            clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-circle-check', 'fa-undo-alt');
+            clone.querySelector('p').style.textDecoration = 'line-through';
+        }
+        clone.querySelectorAll('.fa')[0].dataset.id = task.id
+        clone.querySelectorAll('.fa')[1].dataset.id = task.id
+        fragment.appendChild(clone)
+    })
+    listTask.appendChild(fragment)
+};
+
+const btnAdd = (e) => {
+    console.log(e.target.classList.contains('fa-circle-check'));
+    if (e.target.classList.contains('fa-circle-check')) {
+        // console.log(e.target.dataset.id)
+        tasks[e.target.dataset.id].estado = true;
+        addTask();
+        console.log(tasks);
+    }
+
+    if (e.target.classList.contains('fa-circle-xmark')) {
+        delete tasks[e.target.dataset.id];
+        addTask();
+        console.log(tareas);
+    }
+    if (e.target.classList.contains('fa-undo-alt')) {
+        tasks[e.target.dataset.id].estado = false;
+        addTask();
+    }
+    e.stopPropagation()
+}
+
+
+// agregarProducto.addEventListener('click', addTask2);
+// agregarProductoListado.addEventListener("click", addTask2);
--- src/js.js
+++ src/js.js
@@ -7,24 +7,135 @@ let formTitle = document.getElementById("formTitle");
 let formDescripcion = document.getElementById("formDescripcion");
 let addTask = document.getElementById("addTask");
 let listTable = document.getElementById("historyTable");
+let exitButton = document.getElementById("exitButton");
+const formTask = document.getElementById("formTask");
+
+formTask.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("addTask")) {
+        addTask2();
+        console.log(taskProp);
+    }
+    if (e.target.classList.contains("exitButton")) {
+        mostrarListado()
+    }
+})
+
+let agregarProductoListado = document.getElementById("agregarProductoListado");
+
+let addTask2 = () => {
+    pantallaBienvenida.style.display = "none";
+    pantallaFormulario.style.display = "block";
+    pantallaListado.style.display = "none";
+}
 
 // button-accion agregarProducto
 let agregarProducto = document.getElementById("agregarProducto");
-agregarProducto.addEventListener('click', () => {
-    pantallaBienvenida.style.display = "none";
-    pantallaFormulario.style.display = "block";
-})
 
-let mostrarListado = () => {
+agregarProducto.addEventListener('click', addTask2);
+agregarProductoListado.addEventListener("click", addTask2);
+
+
+let mostrarBienvenida = () => {
     pantallaFormulario.style.display = "none";
-    pantallaListado.style.display = "block";
-}
+    pantallaBienvenida.style.display = "block";
+};
 
-addTask.addEventListener("click", () => {
-    let title = formTitle.value;
-    let descripcion = formDescripcion.value;
-    let modelo = `<td>${title}</td>
-                    <td>${descripcion}</td>`;
-    historyTable.innerHTML += modelo;
-    mostrarListado();
-})
+// exitButton.addEventListener("click", mostrarListado);
+
+// addTask.addEventListener("click", () => {
+//     let title = formTitle.value;
+//     let descripcion = formDescripcion.value;
+//     let modelo = `<td>${title}</td>
+//                     <td>${descripcion}</td>`;
+//     let listObj = dataToObj(title, descripcion);
+//     if (title != "" && descripcion != "") {
+//         listTable.innerHTML += modelo;
+//         formTitle.value = "";
+//         formDescripcion.value = "";
+//         saveObj(listObj);
+//         mostrarListado();
+//     } else {
+//         alert("Te falto agregar los campos")
+//     };
+// });
+
+// formu.addEventListener("click", (e) => {
+//     e.preventDefault();
+    
+//     if(e.target.classList.contains("")
+    
+    
+    
+    
+    
+    
+    
+//     let formData = new FormData(formu);
+
+//     let listObj = dataToObj(formData);
+
+//     let title = localStorage.getItem("formTitle");
+//     let descripcion = localStorage.getItem("formDescripcion");
+
+//     document.getElementById("formTitle").value = title;
+//     document.getElementById("formDescripcion").value = descripcion;
+//     // let title = formTitle.value;
+//     // let descripcion = formDescripcion.value;
+//     let modelo = `<td>${title}</td>
+//                     <td>${descripcion}</td>`;
+    
+//     if (title != "" && descripcion != "") {
+//         listTable.innerHTML += modelo;
+//         formTitle.value = "";
+//         formDescripcion.value = "";
+//         saveObj(listObj);
+//         mostrarListado();
+//     } else {
+//         alert("Te falto agregar campos")
+//     };
+//     return {
+//         "formTitle": title,
+//         "formDescripcion": descripcion,
+//     }
+// })
+
+// getNewId = () => {
+//     let lastId = localStorage.getItem("lastId" || "-1");
+//     let newId = JSON.parse(lastId) + 1;
+//     localStorage.setItem("lastId", JSON.stringify(newId))
+//     return newId;
+// };
+
+// dataToObj = (formData) => {
+//     let titleRef = formData.get("formTitle");
+//     let descripcionRef = formData.get("formDescripcion");
+//     let dataId = getNewId();
+//     return {
+//         "formTitle": titleRef,
+//         "formDescripcion": descripcionRef,
+//         "dataId": dataId,
+//     }
+// };
+
+// saveObj = (listObj) => {
+//     let listArray = JSON.parse(localStorage.getItem("transaccionData")) || [];
+//     listArray.push(listObj);
+//     let listArrayJSON = JSON.stringify(listArray);
+//     localStorage.setItem("transaccionData", listArrayJSON);
+// };
+
+// document.addEventListener("DOMContentLoaded", e => {
+//     let objArray = JSON.parse(localStorage.getItem("transaccionData"))
+//     objArray.forEach(
+//         (array) => {
+//             saveObj(array)
+//         }
+//     )
+// });
+
+// insertListTable = (listObj) => {
+//     let listTableRef = document.getElementById("listTable");
+
+
+// }
